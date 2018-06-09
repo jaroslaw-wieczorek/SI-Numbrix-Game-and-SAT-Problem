bc59517a061a4c9af294986fb6f3e49ef9fc5d38
chris642 <krzysztof.orczyk@student.put.poznan.pl>
2018-06-09 15:53:08 +0200

    Added reduced encoding method. Added file writing to Tseitin encoding and fixed some mistakes.

---
+++ reduced_encoding.py
@@ -0,0 +1,129 @@
+import math
+
+import numpy
+from bitstring import BitArray
+
+from board.puzzle import Puzzle
+
+
+class Encoding:
+    def __init__(self, size):
+        self.size = size
+        self.n = size * size
+        self.gb = int(math.ceil(math.log2(self.n)))
+
+    def decode(self, puzzle, inputFile):
+        with open(inputFile, "r") as f:
+            content = f.readlines()[1:]
+            satout = []
+            if len(content) > 1:
+                for line in content:
+                    line = line[2:]
+                    line = line.split()
+                    for x in line:
+                        satout.append(int(x))
+                del satout[-1]
+
+            else:
+                content = content[0]
+                content = content[2:-2]
+                # content = [x.strip() for x in content]
+                content = content.split()
+                satout = [int(x) for x in content]
+            values = []
+            for i in range(1, self.n + 1):
+                ints = []
+                ints = satout[0:self.n]
+                satout = satout[self.n:]
+                # ints <- first x literals from inputFile
+                # remove the first x literals from inputFile
+                # add the output of iConvert(ints) to values
+                values.append(self.iconvert(ints))
+            # use Puzzle as a template to typeset values into output file--
+            answer = numpy.empty((self.size, 0)).tolist()
+            count = 0
+            it = iter(values)
+            for row in answer:
+                for x in range(0, self.size):
+                    row.append(next(it))
+                print(*row, sep=' ')
+                # print("\n")
+
+    def convert(self, ajdi, x):
+        return (ajdi - 1) * self.n + x
+
+    def iconvert(self, ints):  ###############3
+        for i in ints:
+            if i > 0:
+                return ((i - 1) % self.n) + 1
+
+    def exists(self, id):  ###############3
+        out = []
+        for x in range(1, self.n + 1):
+            out.append(self.convert(id, x))
+        return out
+
+    def isunique(self, id):  ###############3
+        CNF = []
+        for x1 in range(1, self.n):
+            for x2 in range(x1, self.n + 1):
+                if x1 != x2:
+                    CNF.append([-1 * self.convert(id, x1), -1 * self.convert(id, x2)])
+        return CNF
+
+    def arenotqual(self, id1, id2):  ###############3
+        CNF = []
+        for x in range(1, self.n + 1):
+            if id1 != id2:
+                CNF.append([-1 * self.convert(id1, x), -1 * self.convert(id2, x)])
+        return CNF
+
+    def precedes(self, jd, ids):  ###########
+        CNF = []
+        for x in range(1, self.n):
+            clause = []
+            clause.append(-1 * self.convert(jd, x))
+            for i in ids:
+                clause.append(self.convert(i.ID, x + 1))
+            CNF.append(clause)
+        return CNF
+
+    def isequal(self, ID, x):  ##########
+        return self.convert(ID, x)
+
+
+    def encode(self, puzzle, outputfile):  ###########
+        CNF = []
+        # No cell has two numbers;
+        for b in puzzle.puzzle:
+            for c in b:
+                # CNF.append(self.isunique(c.ID))
+                for clau in self.isunique(c.ID):
+                    CNF.append(clau)
+        # Every cell except the one with number n does have a successor;
+        for b in puzzle.puzzle:
+            for c in b:
+                ids = puzzle.listNeighbourhood(c.ID)
+                # CNF.append(self.precedes(c.ID, ids))
+                for clau in self.precedes(c.ID, ids):
+                    CNF.append(clau)
+        # Pre-filled numbers are unchanged.
+        for b in puzzle.puzzle:
+            for c in b:
+                if c.value != 0:
+                    CNF.append(self.isequal(c.ID, c.value))
+
+        with open(outputfile, 'w') as file:
+            file.write("p cnf ")
+            file.write(str(self.n * self.n))
+            file.write(" ")
+            file.write(str(len(CNF)))
+            file.write("\n")
+            for clauso in CNF:
+                clauso = str(clauso).replace("[", "")
+                clauso = str(clauso).replace("]", "")
+                clauso = str(clauso).replace(",", "")
+                # print(clauso)
+                file.write(clauso)
+                file.write(" 0")
+                file.write("\n")
--- tseitisin.py
+++ tseitisin.py
@@ -1,35 +1,63 @@
+import numpy
 from bitstring import BitArray, BitStream
 import math
 import hashlib
 
 class Encoding:
 
-    def __init__(self, n):
-        self.n = n
-        self.hash_table={} #this is dictionary to hash function
-        self.gb = int(math.ceil(math.log2(self.n)))
+    def __init__(self, size):
+        self.size = size
+        self.n = size * size
+        self.hash_table = {} #this is dictionary to hash function
+        self.gb = int(math.ceil(math.log2(self.n))) +1
         self.varnumber = self.gb * self.n
 
     def decode(self, puzzle, inputFile):
-        for i in range(1, puzzle.n):
-            ints = []
-            # ints <- first x literals from inputFile
-            # remove the first x literals from inputFile
-            # add the output of iConvert(ints) to values
+        with open(inputFile, "r") as f:
+            content = f.readlines()[1:]
+            satout = []
+            if len(content) > 1:
+                for line in content:
+                    line = line[2:]
+                    line = line.split()
+                    for x in line:
+                        satout.append(int(x))
+                del satout[-1]
+
+            else:
+                content = content[0]
+                content = content[2:-2]
+                #content = [x.strip() for x in content]
+                content = content.split()
+                satout =[int(x) for x in content]
             values = []
-            values.append(Encoding.iconverse(ints))
-        # use Puzzle as a template to typeset values into output file--
-        print(values)        
+            for i in range(1, self.gb+1):
+                ints = []
+                ints = satout[0:self.n]
+                satout = satout[self.n:]
+                # ints <- first x literals from inputFile
+                # remove the first x literals from inputFile
+                # add the output of iConvert(ints) to values
+                values.append(self.iconverse(ints))
+            # use Puzzle as a template to typeset values into output file--
+            answer = numpy.empty((self.size, 0)).tolist()
+            count = 0
+            it = iter(values)
+            for row in answer:
+                for x in range(0, self.size):
+                    row.append(next(it))
+                print(*row, sep=' ')
+                #print("\n")
 
     def convert(self, iD, x):
         x = x - 1
         ints = []
         xbit = BitArray(uint=x, length=self.gb)
-        for i in range(0, self.gb):
+        for i in range(1, self.gb+1):
             if xbit[-1]:
                 ints.append(((iD - 1) * self.gb + 1))
             else:
-                ints.append(-((iD - 1) * self.gb + 1))
+                ints.append(-1*((iD - 1) * self.gb + 1))
             del xbit[-1]
         return ints
     
@@ -39,8 +67,8 @@ class Encoding:
         power = 1
         for i in ints:
             if i > 0:
-                result += power
-            power *= 2
+                result = result + power
+            power = power * 2
         return result + 1
 
 
@@ -73,22 +101,23 @@ class Encoding:
             CNF.append(clause)
             for i in ints:
                 #add {-h,i} to CNF
-                CNF.append([-h,i])
+                CNF.append([-h, i])
             return CNF
 
 
     def precedes(self, ID, ids):
         CNF = []
-        for x in range(1, self.n):
+        for x in range(1, self.n+1):
             clause = []
             for i in self.convert(ID, x):
                 #add -i to clause
                 clause.append(-i)
             for i in ids:
+                CNF.append(Encoding.tseitin(self, self.convert(i.ID, x + 1)))
                 #add the output of Encoding.Hash(Encoding.convert(i,x + 1)) to clause
                 clause.append(Encoding.hash(self, self.convert(i.ID, x + 1)))
                 #add the output of Tseitin(Convert(i,x + 1)) to CNF
-                CNF.append(Encoding.tseitin(self, self.convert(i.ID, x + 1)))
+
             #add clause to CNF
             CNF.append(clause)
         return CNF
@@ -106,11 +135,27 @@ class Encoding:
         for b in puzzle.puzzle:
             for c in b:
                 ids = puzzle.listNeighbourhood(c.ID)
-                CNF.append(Encoding.precedes(self, c.ID, ids))
+                for clau in Encoding.precedes(self, c.ID, ids):
+                    CNF.append(clau)
+                #CNF.append(Encoding.precedes(self, c.ID, ids))
         for b in puzzle.puzzle:
             for c in b:
                 if c.value != 0:
-                    CNF.append(Encoding.isequal(self, c.ID, c.value))
-        print(CNF)
-        #with open(outputfile, 'w') as file:
-        #    file.write(CNF)
+                    #CNF.append(Encoding.isequal(self, c.ID, c.value))
+                    for clau in self.isequal(c.ID, c.value):
+                        CNF.append(clau)
+        #print(CNF)
+        with open(outputfile, 'w') as file:
+            file.write("p cnf ")
+            file.write(str(self.n*self.n))
+            file.write(" ")
+            file.write(str(len(CNF)))
+            file.write("\n")
+            for clauso in CNF:
+                clauso = str(clauso).replace("[","")
+                clauso = str(clauso).replace("]","")
+                clauso = str(clauso).replace(",","")
+                print(clauso)
+                file.write(clauso)
+                file.write(" 0")
+                file.write("\n")
